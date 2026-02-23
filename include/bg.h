c4b3c8a49468e4a92b71ef0339e4b2c8bd411eb9
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2026-02-23 21:19:36 +0100

    Add separate methods for character and screen data writing.

--- include/bg.h
+++ include/bg.h
@@ -44,22 +44,20 @@
 
 #define BG0_SCREEN_BASE 29
 #define BG1_SCREEN_BASE 30
-#define BG2_SCREEN_BASE 31
 
 #define MEM_BG0_SCREEN_BLOCK ((uint16_t*)(MEM_VRAM + BG0_SCREEN_BASE * 0x800))
 #define MEM_BG1_SCREEN_BLOCK ((uint16_t*)(MEM_VRAM + BG1_SCREEN_BASE * 0x800))
-#define MEM_BG2_SCREEN_BLOCK ((uint16_t*)(MEM_VRAM + BG2_SCREEN_BASE * 0x800))
 
 #define BG0_CHARACTER_BASE 0
 #define BG1_CHARACTER_BASE 1
-#define BG2_CHARACTER_BASE 2
 
 #define MEM_BG0_CHARACTER_BLOCK ((uint16_t*)(MEM_VRAM + BG0_CHARACTER_BASE * 0x4000))
 #define MEM_BG1_CHARACTER_BLOCK ((uint16_t*)(MEM_VRAM + BG1_CHARACTER_BASE * 0x4000))
-#define MEM_BG2_CHARACTER_BLOCK ((uint16_t*)(MEM_VRAM + BG2_CHARACTER_BASE * 0x4000))
 
 #define OBJ_CHARACTER_BLOCK ((uint16_t*)(MEM_VRAM + 0x10000))
 
 void init_bg();
+void set_character_data(uint8_t bg, void* start, uint16_t size);
+void set_screen_data(uint8_t bg, void* start, uint16_t size);
 
 #endif //GROOVEBOY_BG_H
--- src/bg.cpp
+++ src/bg.cpp
@@ -1,29 +1,51 @@
 #include "bg.h"
 #include "dma.h"
 
-extern uint16_t* BG00_character_data_start;
+extern uint16_t *BG00_character_data_start;
 extern uint16_t BG00_character_data_size;
 
-extern uint16_t* BG01_character_data_start;
+extern uint16_t *BG01_character_data_start;
 extern uint16_t BG01_character_data_size;
 
-extern uint16_t* empty_art_BG00_screen_data_start;
+extern uint16_t *empty_art_BG00_screen_data_start;
 extern uint16_t empty_art_BG00_screen_data_size;
 
-extern uint16_t* empty_art_BG01_screen_data_start;
+extern uint16_t *empty_art_BG01_screen_data_start;
 extern uint16_t empty_art_BG01_screen_data_size;
 
 void init_bg() {
     REG_DISPLAY = BG_MODE_0 | SCREEN_DISPLAY_BG0 | SCREEN_DISPLAY_BG1 |
-                SCREEN_DISPLAY_BG2 | SCREEN_DISPLAY_OBJ |
-                ONE_DIMENSIONAL_OBJ_CHARACTER_VRAM_MAPPING;
-    BG0CNT = 0 | BGCNT_SCREEN_SIZE_0 | BG0_CHARACTER_BASE << 2 | BG0_SCREEN_BASE << 8 | 0;
+                  SCREEN_DISPLAY_OBJ | ONE_DIMENSIONAL_OBJ_CHARACTER_VRAM_MAPPING;
     BG1CNT = 0 | BGCNT_SCREEN_SIZE_0 | BG1_CHARACTER_BASE << 2 | BG1_SCREEN_BASE << 8 | 1;
-    BG2CNT = 0 | BGCNT_SCREEN_SIZE_0 | BG2_CHARACTER_BASE << 2 | BG2_SCREEN_BASE << 8 | 2;
+    BG0CNT = 0 | BGCNT_SCREEN_SIZE_0 | BG0_CHARACTER_BASE << 2 | BG0_SCREEN_BASE << 8 | 0;
 
-    dma_push(0, &BG00_character_data_start, MEM_BG0_CHARACTER_BLOCK, BG00_character_data_size);
-    dma_push(0, &BG01_character_data_start, MEM_BG1_CHARACTER_BLOCK, BG01_character_data_size);
+    set_character_data(0, &BG00_character_data_start, BG00_character_data_size);
+    set_character_data(1, &BG01_character_data_start, BG01_character_data_size);
 
-    dma_push(0, &empty_art_BG00_screen_data_start, MEM_BG0_SCREEN_BLOCK, empty_art_BG00_screen_data_size);
-    dma_push(0, &empty_art_BG01_screen_data_start, MEM_BG1_SCREEN_BLOCK, empty_art_BG01_screen_data_size);
+    set_screen_data(0, &empty_art_BG00_screen_data_start, empty_art_BG00_screen_data_size);
+    set_screen_data(1, &empty_art_BG01_screen_data_start, empty_art_BG01_screen_data_size);
+}
+
+void set_character_data(const uint8_t bg, void* start, const uint16_t size) {
+    switch (bg) {
+        case 0:
+            dma_push(0, start, MEM_BG0_CHARACTER_BLOCK, size);
+            break;
+        case 1:
+            dma_push(0, start, MEM_BG1_CHARACTER_BLOCK, size);
+            break;
+        default: break;
+    }
+}
+
+void set_screen_data(const uint8_t bg, void* start, const uint16_t size) {
+    switch (bg) {
+        case 0:
+            dma_push(0, start, MEM_BG0_SCREEN_BLOCK, size);
+            break;
+        case 1:
+            dma_push(0, start, MEM_BG1_SCREEN_BLOCK, size);
+            break;
+        default: break;
+    }
 }
